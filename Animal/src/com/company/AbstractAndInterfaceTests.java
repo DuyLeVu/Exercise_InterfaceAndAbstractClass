38eb40b9cbf7ba5e3047803bbeadc153403da5b2
DuyLeVu <levuduy132@gmail.com>
2021-10-16 16:35:17 +0700

    Add animal

---
+++ Animal/src/com/company/AbstractAndInterfaceTests.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import com.company.animal.Animal;
+import com.company.animal.Chicken;
+import com.company.animal.Tiger;
+import com.company.edible.Edible;
+import com.company.fruit.Apple;
+import com.company.fruit.Fruit;
+import com.company.fruit.Orange;
+
+public class AbstractAndInterfaceTests {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals) {
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken) {
+                Edible edibler = (Chicken) animal;
+                System.out.println(edibler.howToEat());
+            }
+        }
+
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
+    }
+}
---
+++ Animal/src/com/company/animal/Animal.java
@@ -0,0 +1,7 @@
+package com.company.animal;
+
+import com.company.edible.Edible;
+
+public abstract class Animal  {
+    public abstract String makeSound();
+}
